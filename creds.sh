4ab3aa97af5b05e62f5e7415496bcac54a4aaf41
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-06-20 13:32:17 +0530

    creds file works at basic level

--- creds.sh
+++ creds.sh
@@ -22,13 +22,13 @@ done
 shift $((OPTIND-1)) # remove parsed options and args from $@ list
 
 set -eu -o pipefail
-name=$(echo "$output" | tr // -)
-OUTFILE="creds-${name}.txt"
+name=$(echo "$output" | sed 's/\//__/g' | sed 's/-/_/g')
+OUTFILE="creds__${name}.txt"
 declare -A DATA
 
 rm -rf "$OUTFILE"
 
-SSM_PARAMS=("DATABASE_HOST" "DATABASE_PORT" "DATABASE_PASSWORD" "DATABASE_USERNAME" "REDIS_HOST" "REDIS_PORT")
+SSM_PARAMS=("DATABASE_HOST" "DATABASE_PORT" "DATABASE_MASTER_PASSWORD" "DATABASE_MASTER_USERNAME" "REDIS_HOST" "REDIS_PORT")
 for x in "${SSM_PARAMS[@]}"
 do
   d=$(aws ssm get-parameter --with-decryption --name "/$input/$x" | jq -r .Parameter.Value)
@@ -60,10 +60,10 @@ new_next_usable=$(echo "$next_usable" + "1" | bc)
 
 declare -A OUT
 
-OUT['DATABASE_NAME']="db-$name"
+OUT['DATABASE_NAME']="db__$name"
 OUT['DATABASE_HOST']="${DATA['DATABASE_HOST']}"
 OUT['DATABASE_PORT']="${DATA['DATABASE_PORT']}"
-OUT['DATABASE_USER']="user-$name"
+OUT['DATABASE_USER']="user__$name"
 OUT['DATABASE_PASS']=$(echo $RANDOM | md5sum | head -c 20; echo;)
 OUT['REDIS_HOST']="${DATA['REDIS_HOST']}"
 OUT['REDIS_PORT']="${DATA['REDIS_PORT']}"
@@ -73,13 +73,18 @@ OUT['REDIS_DB']="$next_usable"
 
 # Requires postgresql-client-common and postgresql-client-<target-version> to be installed
 
-PGPASSWORD="${DATA['DATABASE_PASSWORD']}" createdb -h "${DATA['DATABASE_HOST']}" -p "${DATA['DATABASE_PORT']}" -U "${DATA['DATABASE_USERNAME']}" "${OUT['DATABASE_NAME']}"
-PGPASSWORD="${DATA['DATABASE_PASSWORD']}" createuser -h "${DATA['DATABASE_HOST']}" -p "${DATA['DATABASE_PORT']}" -U "${DATA['DATABASE_USERNAME']}" "${OUT['DATABASE_USER']}"
-PGPASSWORD="${DATA['DATABASE_PASSWORD']}" psql -h "${DATA['DATABASE_HOST']}" -p "${DATA['DATABASE_PORT']}" -U "${DATA['DATABASE_USERNAME']}" -c "ALTER USER ${OUT['DATABASE_USER']} WITH ENCRYPTED PASSWORD \'${OUT['DATABASE_PASS']}\'; GRANT ALL PRIVILEGES ON DATABASE ${OUT['DATABASE_NAME']} TO ${OUT['DATABASE_USER']};"
+PGPASSWORD="${DATA['DATABASE_MASTER_PASSWORD']}" createdb -h "${DATA['DATABASE_HOST']}" -p "${DATA['DATABASE_PORT']}" -U "${DATA['DATABASE_MASTER_USERNAME']}" "${OUT['DATABASE_NAME']}"
+PGPASSWORD="${DATA['DATABASE_MASTER_PASSWORD']}" createuser -h "${DATA['DATABASE_HOST']}" -p "${DATA['DATABASE_PORT']}" -U "${DATA['DATABASE_MASTER_USERNAME']}" "${OUT['DATABASE_USER']}"
+PGPASSWORD="${DATA['DATABASE_MASTER_PASSWORD']}" psql -h "${DATA['DATABASE_HOST']}" -p "${DATA['DATABASE_PORT']}" -U "${DATA['DATABASE_MASTER_USERNAME']}" -c "ALTER USER ${OUT['DATABASE_USER']} WITH ENCRYPTED PASSWORD '${OUT['DATABASE_PASS']}'; GRANT ALL PRIVILEGES ON DATABASE ${OUT['DATABASE_NAME']} TO ${OUT['DATABASE_USER']};"
 
 redis-cli -h "${DATA['REDIS_HOST']}" -p "${DATA['REDIS_PORT']}" -n "$REDIS_BOOKKEEPING_DB" SET "$REDIS_NEXT_KEY" "$new_next_usable"
 
 
-# for x in "${!OUT[@]}"; do
-#   echo "$x=${OUT[$x]}" >> "$OUTFILE"
-# done
+for x in "${!OUT[@]}"; do
+  echo "$x=${OUT[$x]}" >> "$OUTFILE"
+  aws ssm put-parameter \
+      --name "/$output/$x" \
+      --value "${OUT[$x]}" \
+      --type "SecureString"
+done
+
